1555764ecf38079a2dc822e0c4c46657117ec1c8
atesin <atesin@TITAN.lan>
2018-10-16 22:57:31 -0300

    v1 initial release

---
+++ src/cl/netgamer/clicksortlite/MainPlugin.java
@@ -0,0 +1,15 @@
+
+package cl.netgamer.clicksortlite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class MainPlugin extends JavaPlugin
+{
+
+	@Override
+	public void onEnable()
+	{
+		getLogger().info("clicksort lite loaded");
+	}
+}
